84105e7c1ebcdb4de59e55c448c6017221a97862
Abraham Terfie <abrahamterfie4563@gmail.com>
2022-04-26 12:39:03 -0400

    firs commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     this is the first one 
+    </div>
+  );
+}
+
+export default App;
